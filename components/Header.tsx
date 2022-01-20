c27b5de4f2a3182a1cd9858a1b8229b3af0a9f8f
Sahinur Islam <infosahinur@gmail.com>
2022-01-20 11:27:03 +0600

    banar done

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,34 @@
-import React from 'react';
+import Link from "next/link";
+import React from "react";
 
 function Header() {
-  return <div>
-      <h1>i am a header</h1>
-  </div>;
+  return (
+    <header className="flex justify-between p-5 max-w-7xl mx-auto">
+      <div className="flex items-center space-x-5">
+        <Link href="/">
+          <img
+            className="w-44 object-contain cursor-pointer"
+            src="https://links.papareact.com/yvf"
+            alt=""
+          />
+        </Link>
+        <div className="hidden md:inline-flex items-center space-x-5">
+          <h3 className="cursor-pointer">About</h3>
+          <h3 className="cursor-pointer">Contact</h3>
+          <h3 className="text-white cursor-pointer bg-green-600 px-4 py-1 rounded-full">
+            Follow
+          </h3>
+        </div>
+      </div>
+
+      <div className="flex items-center space-x-5 text-green-600">
+        <h3 className="cursor-pointer">Sign In</h3>
+        <h3 className="border px-4 py-1 rounded-full border-green-600 hover:bg-green-600 hover:text-white cursor-pointer">
+          Get Started
+        </h3>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,36 @@
-import Head from 'next/head'
-import Header from '../components/Header'
+import Head from "next/head";
+import Header from "../components/Header";
 
 export default function Home() {
   return (
-    <div className="">
+    <div className="max-w-7xl mx-auto">
       <Head>
         <title>Medium 2.0</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <Header />
+
+      <div className="flex justify-between items-center bg-yellow-400 border-y border-black py-10 lg:py-0">
+        <div className="px-10 space-y-5">
+          <h1 className="text-6xl max-w-xl font-serif">
+            <span className="underline decoration-black decoration-4">
+              Medium
+            </span>{" "}
+            is a place to write, read and connect
+          </h1>
+          <h2 className="">
+            It's easy and free to post your thinking on any topic and connect
+            with millions of readers
+          </h2>
+        </div>
+
+        <img
+          className="hidden md:inline-flex h-32 lg:h-full"
+          src="https://accountabilitylab.org/wp-content/uploads/2020/03/Medium-logo.png"
+          alt=""
+        />
+      </div>
     </div>
-  )
+  );
 }
