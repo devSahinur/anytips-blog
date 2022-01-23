6002d2e9689662eb44ed1dbc004bf0b52d7a7d9e
Sahinur Islam <infosahinur@gmail.com>
2022-01-24 03:04:39 +0600

    post slug page working

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -1,6 +1,73 @@
+import { GetStaticProps } from "next";
+import Header from "../../components/Header";
+import { sanityClient } from "../../sanity";
+import { Post } from "../../typings";
 
-function Post() {
-  return <div></div>;
+interface Props {
+  post: Post;
+}
+
+function Post({ post }: Props) {
+  console.log(post);
+  return (
+    <main>
+      <Header />
+    </main>
+  );
 }
 
 export default Post;
+
+export const getStaticPaths = async () => {
+  const query = `*[_type == "post"]{
+        _id,
+        slug{
+        current
+        }
+      }`;
+
+  const posts = await sanityClient.fetch(query);
+
+  const paths = posts.map((post: Post) => ({
+    params: {
+      slug: post.slug.current,
+    },
+  }));
+
+  return {
+    paths,
+    fallback: "blocking",
+  };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const query = `*[_type == "post" && slug.current == $slug][0]{
+        _id,
+       _createAt,
+        title,
+        author-> {
+        name,
+        image
+      },
+      description,
+      slug,
+      mainImage 
+    }`;
+
+  const post = await sanityClient.fetch(query, {
+    slug: params?.slug,
+  });
+
+  if (!post) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: {
+      post,
+    },
+    revalidate: 60 // after 60 seconds itll update the old cached version
+  };
+};
