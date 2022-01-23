8c1b572794b7dc8392d8f1bd33aa03cf7f594978
Sahinur Islam <infosahinur@gmail.com>
2022-01-24 01:35:38 +0600

    update

---
+++ sanity.js
@@ -0,0 +1,16 @@
+import {
+  createImageUrlBuilder,
+  createCurrentUserHook,
+  createClient,
+} from "next-sanity";
+
+export const config = {
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  apiVersion: "2021-03-25",
+  useCdn: process.env.NODE_ENV === "production",
+};
+
+export const sanityClient = createClient(config);
+
+export const urlFor = (source) => createImageUrlBuilder(config).image(source);
