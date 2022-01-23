967670945ea385b89ca96aaef7483b5c32d98328
Sahinur Islam <infosahinur@gmail.com>
2022-01-24 02:32:55 +0600

    post card done

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,14 @@
 import Head from "next/head";
+import Link from "next/link";
 import Header from "../components/Header";
+import { sanityClient, urlFor } from "../sanity";
+import { Post } from "../typings";
 
-export default function Home() {
+interface Props {
+  posts: [Post];
+}
+
+export default function Home({ posts }: Props) {
   return (
     <div className="max-w-7xl mx-auto">
       <Head>
@@ -31,6 +38,57 @@ export default function Home() {
           alt=""
         />
       </div>
+
+      {/* Posts */}
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 md:gap-6 p-2 md:p-6">
+        {posts.map((post) => (
+          <Link key={post._id} href={`/post/${post.slug.current}`}>
+            <div className="cursor-pointer group border rounded-lg overflow-hidden">
+              <img
+                className="h-60 w-full object-cover group-hover:scale-105 transition-transform duration-200 ease-out"
+                src={urlFor(post.mainImage).url()!}
+                alt=""
+              />
+              <div className="flex justify-between p-5 bg-white">
+                <div>
+                  <p className="text-lg font-bold">{post.title}</p>
+                  <p className="text-xs">
+                    {post.description} by {post.author.name}
+                  </p>
+                </div>
+
+                <img
+                  className="w-12 h-12 rounded-full"
+                  src={urlFor(post.author.image).url()!}
+                  alt=""
+                />
+              </div>
+            </div>
+          </Link>
+        ))}
+      </div>
     </div>
   );
 }
+
+export const getServerSideProps = async () => {
+  const query = `*[_type == "post"]{
+    _id,
+    title,
+    author-> {
+    name,
+    image
+  },
+  description,
+  slug,
+  mainImage
+  }`;
+
+  const posts = await sanityClient.fetch(query);
+
+  return {
+    props: {
+      posts,
+    },
+  };
+};
---
+++ pages/post/[slug].tsx
@@ -0,0 +1,6 @@
+
+function Post() {
+  return <div></div>;
+}
+
+export default Post;
