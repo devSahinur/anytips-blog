ca2a52f599485c362ef331b7c94003f990554c65
Sahinur Islam <infosahinur@gmail.com>
2022-02-12 05:38:33 +0600

    comment condition fix

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -169,20 +169,21 @@ function Post({ post }: Props) {
           />
         </form>
       )}
-
-      {/* Comment */}
-      <div className="flex flex-col p-10 max-w-2xl mx-auto shadow-yellow-500 shadow space-y-2">
-        <h3 className=" text-4xl">Comments</h3>
-        <hr className="pb-2" />
-        {post.comments.map((c) => (
-          <div key={c._id}>
-            <p>
-              {" "}
-              <span className="text-yellow-500">{c.name}:</span> {c.comment}
-            </p>
-          </div>
-        ))}
+  {
+    post.comments.length >= 1 && <div className="flex flex-col p-10 max-w-2xl mx-auto shadow-yellow-500 shadow space-y-2">
+    <h3 className=" text-4xl">Comments</h3>
+    <hr className="pb-2" />
+    {post.comments.map((c) => (
+      <div key={c._id}>
+        <p>
+          {" "}
+          <span className="text-yellow-500">{c.name}:</span> {c.comment}
+        </p>
       </div>
+    ))}
+  </div>
+  }
+      
     </main>
   );
 }
